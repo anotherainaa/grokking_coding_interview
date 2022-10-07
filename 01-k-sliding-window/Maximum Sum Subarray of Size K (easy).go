58657a4f9e4473ff0d5c0c0132972456b4d68b5a
Ainaa Azman <ainaasakinah@gmail.com>
2022-10-07 15:27:31 +0900

    practice max sum subarray of size k in go

--- 01-k-sliding-window/Maximum Sum Subarray of Size K (easy).go
+++ 01-k-sliding-window/Maximum Sum Subarray of Size K (easy).go
@@ -16,18 +16,45 @@ Input: [2, 3, 4, 1, 5], k=2
 Output: 7
 Explanation: Subarray with maximum sum is [3, 4].
 
+Input
+- array and K value representing the maximum sub-array number
+Output
+- output array representing the maximum sum
+
+
+Approach
+- K Sliding window
+
+subtract the element going out the window
+add the new elemnt and compare max.
+
 
 
 */
+
 import (
 	"fmt"
+	"math"
 )
 
-func maxSubArray() {
-	fmt.Println("hi")
+func maxSubArray(nums []int, k int) int {
+	maxSum := 0
+	windowSum := 0
+	windowStart := 0
+
+	for windowEnd := 0; windowEnd < len(nums); windowEnd++ {
+		windowSum += nums[windowEnd]
+
+		if windowEnd >= k-1 {
+			maxSum = int(math.Max(float64(maxSum), float64(windowSum)))
+			windowSum -= nums[windowStart]
+			windowStart++
+		}
+	}
+
+	return maxSum
 }
 
 func main() {
-	maxSubArray()
-	fmt.Println("Hello World")
+	fmt.Println(maxSubArray([]int{2, 1, 5, 1, 3, 2}, 3))
 }
