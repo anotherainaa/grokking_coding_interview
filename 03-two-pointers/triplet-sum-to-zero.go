8f2840bf55c50f2aad8105055d0a4dfccbd4cd76
Ainaa Azman <ainaasakinah@gmail.com>
2022-10-14 18:01:15 +0900

    add triplet sumt o zero

--- 03-two-pointers/triplet-sum-to-zero.go
+++ 03-two-pointers/triplet-sum-to-zero.go
@@ -27,14 +27,39 @@ approach
 General idea
 - sort the slice
 - take the first number and find the pair from remanining numbers that equal to zero when added with first number
+- skip duplicate numbers as it will produce duplicate triplets
 - return all the triplets
 
 this one is a O(N2)
 
 */
 
-func searchPair() {
+func searchPair(nums []int, target int, left int, triplets [][]int) [][]int {
+	right := len(nums) - 1
 
+	for left < right {
+		currentSum := nums[left] + nums[right]
+
+		if currentSum == target { // found the triplet
+			triplets = append(triplets, []int{-target, nums[left], nums[right]})
+			left += 1
+			right -= 1
+			for left < right && nums[left] == nums[left-1] {
+				left += 1
+			}
+
+			for right < left && nums[right] == nums[right+1] {
+				right -= 1
+			}
+
+		} else if target > currentSum {
+			left += 1
+		} else {
+			right -= 1
+		}
+	}
+
+	return triplets
 }
 
 func sumToZero(nums []int) [][]int {
@@ -42,9 +67,19 @@ func sumToZero(nums []int) [][]int {
 		return nums[i] < nums[j]
 	})
 
-	fmt.Println(nums)
+	results := [][]int{}
+	var p = &results
+	fmt.Println(*p)
 
-	return [][]int{[]int{0, 0, 0}}
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		results = searchPair(nums, -nums[i], i+1, results)
+	}
+	fmt.Println(&results)
+
+	return results
 }
 
 func main() {
