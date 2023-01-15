90ad263c0107d4d3f51af72c8c9da4f7dd74e9d3
Ainaa Azman <ainaasakinah@gmail.com>
2023-01-15 21:38:06 +0900

    add fruit basket problem

---
+++ 02-k-sliding-window/Fruits into Baskets (medium).go
@@ -0,0 +1,30 @@
+package main
+
+/*
+Problem Statement
+You are visiting a farm to collect fruits. The farm has a single row of fruit trees. You will be given two baskets, and your goal is to pick as many fruits as possible to be placed in the given baskets.
+
+You will be given an array of characters where each character represents a fruit tree. The farm has following restrictions:
+
+Each basket can have only one type of fruit. There is no limit to how many fruit a basket can hold.
+You can start with any tree, but you can’t skip a tree once you have started.
+You will pick exactly one fruit from every tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+Write a function to return the maximum number of fruits in both baskets.
+
+
+Example 1:
+Input: Fruit=['A', 'B', 'C', 'A', 'C']
+Output: 3
+Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+
+- Mental model is to keep picking fruit, if there are 3 unique letters, we gotta remove the one infront.
+
+Data Structure
+
+- we return a subarray
+- we can use a variable to count unique characters.
+- we need a hash map to check things at O(1) efficiency.
+- we use the keys to check how many types of fruits do we have.
+
+
+*/
--- 02-k-sliding-window/Problem Challenge 2: String Anagrams (hard).go
+++ 02-k-sliding-window/Problem Challenge 2: String Anagrams (hard).go
@@ -3,7 +3,33 @@ package main
 import "fmt"
 
 /*
-comment here
+Problem Statement
+
+- Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+Input: String="ppqp", Pattern="pq"
+Output: [1, 2]
+Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+
+Input: String="abbcabc", Pattern="abc"
+Output: [2, 3, 4]
+Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+
+apporach 
+- sliding window 
+
+- keep a char frequency of pattern 
+- keep a char frequency to check the window 
+- keep adding characters to window
+- add the frequency of character 
+
+- if longer than pattern length, remove a character from the front and the frequency of characters
+
+- check if the length is the same as pattern
+- for each character, check that each character is equivalent to pattern characters. 
+
+- if true, add index to array 
+
 */
 
 func main() {
