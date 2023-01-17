79b88a72acd2ada6b5dd09281bbd4bdc0bfa572e
Ainaa Azman <ainaasakinah@gmail.com>
2023-01-17 16:36:35 +0900

    add longest one

--- 02-k-sliding-window/Longest Subarray with Ones after Replacement (hard).js
+++ 02-k-sliding-window/Longest Subarray with Ones after Replacement (hard).js
@@ -23,12 +23,49 @@ Output- longest subarray with ones if you are allowed to replace k amount of os 
 Example 1 - Longest One. 
 6 because we replace 0 at index 5 
 
-
-
-
 Approach 
+
+0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1
+               l
+                              r
+k  = 2
+
+
+- keep adding to window
+- if 0 is more than k, 
+ - remove one letter from the beginning
+ - check the max length between the one before current 0, 
+
 */
 
 function longestOnesAfterReplacement(numbers, k) {
+  let maxLength = 0;
+  let windowStart = 0;
+  let numFrequency = {};
 
+  for (let windowEnd = 0; windowEnd < numbers.length; windowEnd += 1) {
+    let rightNum = numbers[windowEnd];
+
+    if (!numFrequency[rightNum]) {
+      numFrequency[rightNum] = 0;
+    }
+    numFrequency[rightNum] += 1;
+
+    // while there are more 0s than k, 
+    // we need to remove the letter on the left until we've removed enough zeros
+    while (numFrequency[0] > k) {
+      let leftNum = numbers[windowStart];
+      numFrequency[leftNum] -= 1
+      if (numFrequency[leftNum] === 0) {
+        delete numFrequency[leftNum]
+      }
+      windowStart++
+    }
+
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
 }
+
+console.log(longestOnesAfterReplacement([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2));
+console.log(longestOnesAfterReplacement([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3));
